c0c10439be6a0fec08b603eb3bf9117aa9cec0da
kburk1997 <kburk97@optonline.net>
2016-09-13 15:49:01 -0400

    Start rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BettereBinder::Application.config.secret_key_base = '5ec1a874436010411e673bf16874410e38ef33bf377b0c69abb9c37c39558d8422e5c7d45478778e2db16b6b2574aa81454f1015933bd3371a89a0a1d0a41131'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BettereBinder::Application.config.session_store :cookie_store, key: '_BettereBinder_session'
