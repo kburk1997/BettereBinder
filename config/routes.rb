558f82eed575949ea25ae054624fb3622f94f996
kburk1997 <kburk97@optonline.net>
2016-09-27 16:04:28 -0400

    Configure root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ BettereBinder::Application.routes.draw do
   resources :binders
 
   get "static_pages/home"
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
