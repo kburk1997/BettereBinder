32d760082f7fdb155470a73fa7ba72bbf68e0e3e
kburk1997 <kburk97@optonline.net>
2016-10-11 13:35:46 -0400

    ALL UNIT TESTS PASSED (for now)

--- test/controllers/documents_controller_test.rb
+++ test/controllers/documents_controller_test.rb
@@ -8,44 +8,44 @@ class DocumentsControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
-    get :index
+    get :index, :binder_id=> @binder.id, :tab_id=> @tab.id
     assert_response :success
     assert_not_nil assigns(:documents)
   end
 
   test "should get new" do
-    get :new
+    get :new, :binder_id=> @binder.id, :tab_id=> @tab.id
     assert_response :success
   end
 
   test "should create document" do
     assert_difference('Document.count') do
-      post :create, document: { description: @document.description, document_url: @document.document_url, tab_id: @document.tab_id, title: @document.title }
+      post :create, :binder_id=> @binder.id, :tab_id=> @tab.id, document: { description: @document.description, document_url: @document.document_url, tab_id: @document.tab_id, title: @document.title }
     end
 
-    assert_redirected_to document_path(assigns(:document))
+    assert_redirected_to binder_tab_document_path(@binder,@tab,assigns(:document))
   end
 
   test "should show document" do
-    get :show, id: @document
+    get :show, :binder_id=> @binder.id, :tab_id=> @tab.id, id: @document
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @document
+    get :edit, :binder_id=> @binder.id, :tab_id=> @tab.id, id: @document
     assert_response :success
   end
 
   test "should update document" do
-    patch :update, id: @document, document: { description: @document.description, document_url: @document.document_url, tab_id: @document.tab_id, title: @document.title }
-    assert_redirected_to document_path(assigns(:document))
+    patch :update, :binder_id=> @binder.id, :tab_id=> @tab.id, id: @document, document: { description: @document.description, document_url: @document.document_url, tab_id: @document.tab_id, title: @document.title }
+    assert_redirected_to binder_tab_document_path(@binder,@tab,assigns(:document))
   end
 
   test "should destroy document" do
     assert_difference('Document.count', -1) do
-      delete :destroy, id: @document
+      delete :destroy, :binder_id=> @binder.id, :tab_id=> @tab.id, id: @document
     end
 
-    assert_redirected_to documents_path
+    assert_redirected_to binder_tab_documents_path(@binder,@tab)
   end
 end
