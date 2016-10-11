d88c609fe939a10247447d98b1dfa5f04bd78b74
kburk1997 <kburk97@optonline.net>
2016-10-11 13:26:10 -0400

    finish document model

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,20 +1,24 @@
 class DocumentsController < ApplicationController
+  before_action :set_tabs
+
   before_action :set_document, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
   def index
     @documents = Document.all
+
     respond_with(@documents)
   end
 
   def show
-    respond_with(@document)
+    respond_with(@binder,@tab,@document)
   end
 
   def new
     @document = Document.new
-    respond_with(@document)
+
+    respond_with(@binder,@tab, @document)
   end
 
   def edit
@@ -23,22 +27,27 @@ class DocumentsController < ApplicationController
   def create
     @document = Document.new(document_params)
     @document.save
-    respond_with(@document)
+    respond_with(@binder,@tab,@document)
   end
 
   def update
     @document.update(document_params)
-    respond_with(@document)
+    respond_with(@binder,@tab,@document)
   end
 
   def destroy
     @document.destroy
-    respond_with(@document)
+    respond_with(@binder,@tab,@document)
   end
 
   private
     def set_document
       @document = Document.find(params[:id])
+    end
+
+    def set_tabs
+      @tab=Tab.find(params[:tab_id])
+      @binder=Binder.find(params[:binder_id])
     end
 
     def document_params
