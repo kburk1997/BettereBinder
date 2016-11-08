8aa26799574ceb23e28b459cd5488c6955ba6559
kburk1997 <kburk97@optonline.net>
2016-11-08 17:06:05 -0500

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ BettereBinder::Application.routes.draw do
 
   get 'my_binders' => 'binders#user'
   get "static_pages/home"
+
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
