80af3d1fc693ff3e0cbed7a08e136d72a4432bd6
kburk1997 <kburk97@optonline.net>
2016-11-08 16:36:23 -0500

    finish issue #10

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,7 +5,7 @@ module DeviseHelper
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
     <div class="alert alert-error alert-danger"> <button type="button"
-    class="close" data-dismiss="alert">×</button>
+    class="close" data-dismiss="alert">x</button>
       #{messages}
     </div>
     HTML
